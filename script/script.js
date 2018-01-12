c0a6e35a4dd7d37b8b1ba1e9a67e15b19e94e5bb
soapluke <lukaslindgren@gmail.com>
2018-01-12 13:00:43 +0100

    scrolltop js, startpagegrid o css

---
+++ script/script.js
@@ -0,0 +1,14 @@
+//Knapp för att komma till toppen av sidan
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $('#scroll').fadeIn();
+        } else {
+            $('#scroll').fadeOut();
+        }
+    });
+    $('#scroll').click(function(){
+        $("html, body").animate({ scrollTop: 0 }, 600);
+        return false;
+    });
+});
